4ca7a48429b98afbb87537a124a07f253daeeaca
zhangxianlei <zhangxianlei>
2018-08-02 21:03:02 +0800

    list css init

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,7 +4,12 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    await this.ctx.render('home/index', {Loader: require('Loader')});
+    let len = 16;
+    let data = [];
+    for (var i = 0; i < len; i++) {
+        data.push(i);
+    }
+    await this.ctx.render('home/index', {data: data, Loader: require('Loader')});
   }
   async index2() {
     let data = await this.ctx.model.User.find({}).exec();
